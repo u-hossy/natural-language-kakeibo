e5a6e680b33ac4e164218465420e7a9da8c312b0
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-09-07 14:05:13 +0900

    枠線を下部だけにした

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./mode-toggle";
 
 export default function Header() {
   return (
-    <header className="flex h-16 flex-row justify-between border border-b px-4">
+    <header className="flex h-16 flex-row justify-between border-b px-4">
       <div className="my-auto text-lg font-bold">
         <span>自然言語計算ツール</span>
       </div>
