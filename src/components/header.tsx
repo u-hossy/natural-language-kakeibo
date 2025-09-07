04c5a12aabb9cc60e048d93a246058b1141e6b3a
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-09-07 12:49:27 +0900

    prettier-plugin-tailwindcssの追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import { ModeToggle } from "./mode-toggle";
 
 export default function Header() {
   return (
-    <header className="px-4 h-16 flex flex-row justify-between border border-b">
-      <div className="font-bold text-lg my-auto">
+    <header className="flex h-16 flex-row justify-between border border-b px-4">
+      <div className="my-auto text-lg font-bold">
         <span>自然言語計算ツール</span>
       </div>
       <div className="my-auto">
