472da408e27d7cf0f30820b6b864c3604e2a019c
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-09-07 17:54:31 +0900

    タイトルなどを変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex h-16 flex-row justify-between border-b px-4">
       <div className="my-auto text-lg font-bold">
-        <a href="/">自然言語内の数字計算ツール</a>
+        <a href="/">自然言語内の数値計算ツール</a>
       </div>
       <div className="my-auto">
         <ModeToggle />
